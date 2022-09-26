bf60b452cea959ebb226f9aec7b2fd6ddc2e763d
Michael Thousand <michael.thousand@outlook.com>
2022-09-26 12:42:23 -0500

    version 2.2.1
    updated typing speed

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var TxtRotate = function(el, toRotate, period) {
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
   
     var that = this;
-    var delta = 300 - Math.random() * 100;
+    var delta = 180 - Math.random() * 100; //180 sets the speed of the typing
   
     if (this.isDeleting) { delta /= 2; }
   
@@ -69,3 +69,4 @@ function newTab() {
 };
 
 resumeButton.addEventListener('click', newTab);
+
